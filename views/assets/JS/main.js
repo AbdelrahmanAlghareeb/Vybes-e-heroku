cf35cd8a4f800376785504520779b47cfe073749
unknown <az.mohamedayman@gmail.com>
2020-10-14 00:16:32 +0200

    release 1.0.2

--- views/assets/JS/main.js
+++ views/assets/JS/main.js
@@ -1,9 +1,22 @@
-$(".default_option").click(function(){
+
+$(function(){
+
+  $(".default_option").on('click',function(){
     $(this).parent().toggleClass("active");
   })
-  
-  $(".select_ul li").click(function(){
+    
+  $(".select_ul li").on('click',function(){
     var currentele = $(this).html();
     $(".default_option li").html(currentele);
     $(this).parents(".select_wrap").removeClass("active");
   })
+
+    /*SIde Menue
+  $('.menu_side_BTn').on('click', function () {
+    console.log('hiiiii')
+    $('.side_menu').toggleClass('open')
+
+  })
+  */
+
+});
